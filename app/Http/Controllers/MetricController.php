b23ce6c927b7cae49989202a75f46be4faafcf62
Bruno Corrêa <bruno.correa.at@gmail.com>
2024-09-25 18:30:17 -0300

    Adicionado pacote L5-Swagger e configuracao inicial para documentacao da API

--- app/Http/Controllers/MetricController.php
+++ app/Http/Controllers/MetricController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use App\Services\MetricServiceInterface;
+use OpenApi\Annotations\OA;
+use OpenApi\Annotations\PathItem;
+use OpenApi\Annotations\Info;
 
+/**
+ * @OA\Info(title="DashTrack API", version="0.1")
+ */
 class MetricController extends Controller
 {
     private MetricServiceInterface $metricService;
@@ -19,6 +25,16 @@ class MetricController extends Controller
         return $this->metricService;
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/info",
+     *     tags={"api"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation"
+     *     )
+     * )
+     */
     public function getInfo(): string
     {
         $service = $this->getMetricService();
@@ -26,6 +42,16 @@ class MetricController extends Controller
         return json_encode($service->getInfo());
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/orders",
+     *     tags={"metric"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation"
+     *     )
+     * )
+     */
     public function getOrders(): string
     {
         $service = $this->getMetricService();
@@ -33,6 +59,16 @@ class MetricController extends Controller
         return json_encode($service->getOrders());
     }
 
+     /**
+     * @OA\Get(
+     *     path="/api/items",
+     *     tags={"metric"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation"
+     *     )
+     * )
+     */
     public function getItems(): string
     {
         $service = $this->getMetricService();
@@ -40,6 +76,16 @@ class MetricController extends Controller
         return json_encode($service->getItems());
     }
 
+     /**
+     * @OA\Get(
+     *     path="/api/invoices",
+     *     tags={"metric"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation"
+     *     )
+     * )
+     */
     public function getInvoices(): string
     {
         $service = $this->getMetricService();
@@ -47,6 +93,16 @@ class MetricController extends Controller
         return json_encode($service->getInvoices());
     }
 
+     /**
+     * @OA\Get(
+     *     path="/api/sellers",
+     *     tags={"metric"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation"
+     *     )
+     * )
+     */
     public function getSellers(): string
     {
         $service = $this->getMetricService();
