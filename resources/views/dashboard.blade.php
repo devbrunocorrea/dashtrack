c6add20a62b75175ecf18305ad63cef712055757
Bruno Corrêa <bruno.correa.at@gmail.com>
2024-09-30 01:28:44 -0300

    Ajustes nos templates para normalização dos indicadores

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,50 +10,58 @@
                     <div class="small-box bg-info">
                         <div class="inner">
                             <h3>{{ $totalOrders }}</h3>
-                            <p>Total Orders</p>
+                            <p>Pedidos</p>
                         </div>
                         <div class="icon">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
                 </div>
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-success">
                     <div class="inner">
-                        <h3>{{ $totalItems }}</h3>
-                        <p>Total Items</p>
+                        <h3>{{ $totalOrdersCanceled }}</h3>
+                        <p>Pedidos Cancelados</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-stats-bars"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
                 </div>
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-warning">
                         <div class="inner">
-                            <h3>{{ $totalSellers }}</h3>
-                            <p>Total Sellers</p>
+                            <h3>{{ $totalOrdersDelivered }}</h3>
+                            <p>Pedidos Entregues</p>
+                        </div>
                         </div>
                         <div class="icon">
                             <i class="ion ion-person-add"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{ $totalOrdersDoneToDelivery }}</h3>
+                            <p>Pedidos Pronto Para Entregar</p>
+                        </div>
+                        </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                        </div>
                 <div class="col-lg-3 col-6">
                  <div class="small-box bg-danger">
                     <div class="inner">
-                        <h3>{{ $totalInvoices }}</h3>
-                        <p>Total Invoices</p>
+                        <h3>{{ $totalOrdersIncompleteData }}</h3>
+                        <p>Pedidos Incompletos</p>
                     </div>
-                    <div class="icon">
-                        <i class="ion ion-pie-graph"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
+        </div>
             <div class="row">
                 <div class="col-md-12">
                     <canvas id="ordersChart" style="width:100%;max-width:700px"></canvas>
@@ -65,13 +73,25 @@
 
 @section('scripts')
 <script>
+    var labels = [];
+    var values = [];
+
+    @foreach ($sales as $label => $value)
+        labels.push("{{ $label }}");
+        values.push({{ $value }});
+    @endforeach
+
+    console.log(labels);
+    console.log(values);
+
     var ctx = document.getElementById('ordersChart').getContext('2d');
     var ordersChart = new Chart(ctx, {
-        type: 'line',
+        type: 'bar',
         data: {
-            labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+            labels: labels,
             datasets: [{
-                data: [10,20,30,10,25],
+                label: 'Vendas',
+                data: values,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -83,5 +103,9 @@
             }]
         }
     });
+
+    setInterval(function(){
+        location.reload();
+    }, 5000);
 </script>
 @endsection
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <a href="#" class="brand-link">
-    <span class="brand-text font-weight-light">#{{ config('app.name', 'Laravel') }}</span>
+    <span class="brand-text font-weight-light"># {{ config('app.name', 'Laravel') }}</span>
   </a>
 
   <div class="user-panel mt-3 pb-3 mb-3 d-flex">
